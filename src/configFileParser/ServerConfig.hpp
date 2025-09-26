1ff485ed30d59cb5b389095752ba8072d55be3e5
Clement Colin <clmnt.co@gmail.com>
2025-09-26 17:59:30 +0900

    made some progress on the config file parser

--- src/configFileParser/ServerConfig.hpp
+++ src/configFileParser/ServerConfig.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 
+#define DEFAULT_CONF "configFiles/default.conf"
+
 class ServerConfig
 {
 public:
@@ -10,7 +12,13 @@ public:
 private:
 
     std::vector<std::string> loadConfigFile(const std::string& path);
-    std::vector<std::string> loadStringConfig(const std::string& string);
+
     void parseConfig(std::vector<std::string>& rawConfig);
-    void ServerConfig::normalizeConfig(std::vector<std::string>& rawConfig);
+
+    std::vector<std::string>& normalizeConfig(std::vector<std::string>& rawConfig);
+    void removeComments(std::vector<std::string>& rawConfig, int i);
+    void trimCollapse(std::vector<std::string>& rawConfig, int i);
+    void splitBrackets(std::vector<std::string>& rawConfig, int i);
+
+    void printVector(const std::vector<std::string>& vec);//debug
 };
--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,15 @@
 #include "http/HttpRequest/HttpRequest.hpp"
 #include "http/HttpResponse/HttpResponse.hpp"
 
+#include "../src/configFileParser/ServerConfig.hpp"
 
 #include "http/http_tester.hpp"
+#include "../src/tests/tests.hpp"
 
-#include "tests.hpp"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    // Tests
-    FileInfo_SafePath_tests("test");
-    FileInfo_SafePath_tests("errors/400.html");
-    FileInfo_SafePath_tests("/");
-    FileInfo_SafePath_tests("/../..");
-
-    testDetectMimeType();
-    //------------------------ This is meant for whoever will review my PR. Feel free to delete when it's merged - Clement
+    ServerConfig config(argc, argv);
 
     std::cout << "Web Server Starting..." << std::endl;
     http_tester();
