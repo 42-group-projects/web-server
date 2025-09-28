320a8756d913794928c649b22979f7c1187ad575
Clement Colin <clmnt.co@gmail.com>
2025-09-28 18:44:32 +0900

    ready to merge

--- scripts/format.sh
+++ scripts/format.sh
@@ -16,25 +16,23 @@ FILES=$(find "$BASE_DIR" -type f \( -iname "*.cpp" -o -iname "*.hpp" \))
 # Apply AStyle formatting with backups
 echo "Running AStyle..."
 for f in $FILES; do
-	# Tell AStyle to put the backup in a temp location first
-	astyle --style=allman --indent=tab \
-	--break-blocks \
-	--pad-oper \
-	--keep-one-line-statements \
-	--keep-one-line-blocks \
-	--pad-include \
-	--unpad-paren \
-	--squeeze-lines=1 \
-	--squeeze-ws \
-	--break-closing-braces \
-	--break-one-line-headers \
-	--remove-braces "$f"
+        astyle --style=allman --indent=tab \
+        --break-blocks \
+        --pad-oper \
+        --keep-one-line-statements \
+        --keep-one-line-blocks \
+        --pad-header \
+        --unpad-paren \
+        --delete-empty-lines \
+        --break-closing-braces \
+        --break-one-line-headers \
+        --remove-braces "$f"
 
-	# Move the .orig to the backup folder if it exists
-	ORIG_FILE="${f}.orig"
-	if [ -f "$ORIG_FILE" ]; then
-		mv "$ORIG_FILE" "$BACKUP_DIR/"
-	fi
+        # Move the .orig to the backup folder if it exists
+        ORIG_FILE="${f}.orig"
+        if [ -f "$ORIG_FILE" ]; then
+                mv "$ORIG_FILE" "$BACKUP_DIR/"
+        fi
 done
 
 echo "AStyle formatting completed. Backups are in '$BACKUP_DIR'."
