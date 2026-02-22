64ab4e2e954b545576d76baf72c0d75bda5c2139
minokakakizaki <minoka.kakizaki@gmail.com>
2026-02-22 14:19:36 +0900

    made a fix so it cgi works with post man

--- src/http/HttpResponse/HttpResonse.cpp
+++ src/http/HttpResponse/HttpResonse.cpp
@@ -90,6 +90,10 @@ HttpResponse &HttpResponse::parseCgiResponse(const std::string& cgi_output)
 
 	while (std::getline(header_stream, line))
 	{
+		// Remove trailing \r if present (std::getline only removes \n)
+		if (!line.empty() && line[line.size() - 1] == '\r')
+			line.erase(line.size() - 1);
+
 		size_t colon_pos = line.find(':');
 		if (colon_pos != std::string::npos)
 		{
