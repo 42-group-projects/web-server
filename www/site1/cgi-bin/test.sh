c956dee41a0938eb791ce61cb0535d663d07bff4
minokakakizaki <minoka.kakizaki@gmail.com>
2026-02-22 14:26:00 +0900

    got rid of keep alive in cgi file

--- www/site1/cgi-bin/test.sh
+++ www/site1/cgi-bin/test.sh
@@ -4,7 +4,5 @@ body="$(env | sort)"
 length=$(printf '%s\n' "$body" | wc -c | awk '{print $1}')
 
 printf 'HTTP/1.1 200 OK\r\n'
-printf 'Content-Type: text/plain\r\n'
-printf 'Connection: close\r\n'
 printf '\r\n'
 printf '%s\n' "$body"
